f6316829e099dc3311a8ebdec1f9335728a0a996
Aditya <adityaprayoga1903@gmail.com>
2025-04-10 17:13:05 +0700

    first_commit

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Team-4
-Quiz 2 - Sanbercode QA Engineer
 
-Aditya (daniel-adit) - Create an Account
-Reyhan Seifan Safero (reyhansafero) - Login
-Winda Vanecya (windavanecya11) - Choose products and Update Shopping Cart
+Testing Register, Login, Forgot Password of Magento Software Testing Web Demo
--- cypress/support/page-object/lumaPage.js
+++ cypress/support/page-object/lumaPage.js
@@ -2,9 +2,14 @@ class lumaPage {
 
     create = '.panel > .header > :nth-child(3) > a'
     confirm = '#form-validate > .actions-toolbar > div.primary > .action > span'
+    login = '.authorization-link > a'
 
     createAccount(){
         cy.get(this.create).click()
+    }
+
+    loginButton(){
+        cy.get(this.login).click()
     }
     confirmButton(){
         cy.get(this.confirm).click()
